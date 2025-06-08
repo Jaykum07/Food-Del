9b0c3106ac706db0f4bada3e22c87cb1956799c6
jaykum07 <kumrawatjay07@gmail.com>
2025-06-08 13:45:30 +0530

    Complete the frontend functionality

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, {useState} from 'react'
 import Navbar from './components/Navbar/navbar'
 import { Route, Routes } from 'react-router-dom'
 import Home from './pages/Home/Home'
@@ -10,12 +10,13 @@ import LoginPopup from './components/LoginPopup/LoginPopup'
 
 const App = () => {
 
-  
+  const [showLogin, setShowLogin] = useState(false)
 
   return (
     <>
+    {showLogin?<LoginPopup setShowLogin={setShowLogin} />:<></>}
       <div className='app'>
-        <Navbar />
+        <Navbar setShowLogin={setShowLogin}/>
         <Routes>
           <Route path='/' element={<Home />} />
           <Route path='/cart' element={<Cart />} />
